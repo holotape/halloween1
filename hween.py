c3e2784e296f467b9b2edf023d6963450918cd72
holotape <mjw555@gmail.com>
2016-10-10 23:17:09 -0400

    Add files via upload

---
+++ hween.py
@@ -0,0 +1,23 @@
+﻿p0p = "░░░░░░░░░░"
+p10p = "▓░░░░░░░░░"
+p20p = "▓▓░░░░░░░░"
+p30p = "▓▓▓░░░░░░░"
+p40p = "▓▓▓▓░░░░░░"
+p50p = "▓▓▓▓▓░░░░░"
+p60p = "▓▓▓▓▓▓░░░░"
+p70p = "▓▓▓▓▓▓▓░░░"
+p80p = "▓▓▓▓▓▓▓▓░░"
+p90p = "▓▓▓▓▓▓▓▓▓░"
+p100p = "▓▓▓▓▓▓▓▓▓▓"
+
+print p0p
+print p10p
+print p20p
+print p30p
+print p40p
+print p50p
+print p60p
+print p70p
+print p80p
+print p90p
+print p100p
