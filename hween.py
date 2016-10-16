923c6c5f3f9f7432944453e14fdbac4377a97367
holotape <mjw555@gmail.com>
2016-10-16 14:13:56 -0400

    Add files via upload

--- hween.py
+++ hween.py
@@ -2,48 +2,48 @@
 
 def experiment_xxx():
    print("Installing XXX...      "),
-   time.sleep(1),
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   time.sleep(1)
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("░░░░░░░░░░   0%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓░░░░░░░░░  10%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓░░░░░░░░  20%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓░░░░░░░  30%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓░░░░░░  40%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓░░░░░  50%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓░░░░  60%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓░░░  70%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓▓░░  80%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓▓▓░  90%"),
-   time.sleep(1),
+   time.sleep(1)
    
-   sys.stderr.write("\x1b[2J\x1b[H"),
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓▓▓▓ 100%"),
    
 experiment_xxx()
