9691caf8cb92252846f0fd7288cba93aeeb636c5
holotape <mjw555@gmail.com>
2016-10-16 15:25:31 -0400

    printf "\033c"

--- hween.py
+++ hween.py
@@ -1,50 +1,50 @@
-﻿import sys, time, os
+﻿import sys, time
 
 def experiment_xxx():
    print("Installing XXX...      "),
    time.sleep(1)
 
-   os.system('clear')
+   printf "\033c"
    print("░░░░░░░░░░   0%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓░░░░░░░░░  10%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓░░░░░░░░  20%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓░░░░░░░  30%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓░░░░░░  40%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓▓░░░░░  50%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓▓▓░░░░  60%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓▓▓▓░░░  70%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓▓▓▓▓░░  80%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓▓▓▓▓▓░  90%"),
    time.sleep(1)
    
-   os.system('clear')
+   printf "\033c"
    print("▓▓▓▓▓▓▓▓▓▓ 100%"),
    
 experiment_xxx()
