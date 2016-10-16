4ff73aae24cffbaba0e2de278554b5686063bf99
holotape <mjw555@gmail.com>
2016-10-16 15:36:08 -0400

    print(chr(27) + "[2J")

--- hween.py
+++ hween.py
@@ -4,47 +4,47 @@ def experiment_xxx():
    print("Installing XXX...      "),
    time.sleep(1)
 
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("░░░░░░░░░░   0%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓░░░░░░░░░  10%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓░░░░░░░░  20%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓░░░░░░░  30%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓░░░░░░  40%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓▓░░░░░  50%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓▓▓░░░░  60%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓▓▓▓░░░  70%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓▓▓▓▓░░  80%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓▓▓▓▓▓░  90%"),
    time.sleep(1)
    
-   print("\033c"),
+   print(chr(27) + "[2J")
    print("▓▓▓▓▓▓▓▓▓▓ 100%"),
    
 experiment_xxx()
