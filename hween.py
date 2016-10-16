7d5c4924fc459a37a25b52540167b276e7ae0a3e
holotape <mjw555@gmail.com>
2016-10-16 18:19:29 -0400

    bar sizing

--- hween.py
+++ hween.py
@@ -18,57 +18,57 @@ def experiment_xxx():
 
    sys.stderr.write("\x1b[2J\x1b[H")
    print("INJECTING X-SERUM")
-   print("░░░░░░░░░░   0%")
+   print("░░░░░░░░░░░░░░░░░░░░   0%")
    time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("INJECTING X-SERUM")
-   print("▓░░░░░░░░░  10%")
+   print("▓▓░░░░░░░░░░░░░░░░░░  10%")
    time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("MEASURING VITALS")
-   print("▓▓░░░░░░░░  20%")
+   print("▓▓▓▓░░░░░░░░░░░░░░░░  20%")
    time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("MEASURING VITALS")
-   print("▓▓▓░░░░░░░  30%")
+   print("▓▓▓▓▓▓░░░░░░░░░░░░░░  30%")
    time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("SUBJECT NON-RESPONSIVE")
-   print("▓▓▓▓░░░░░░  40%")
+   print("▓▓▓▓▓▓▓▓░░░░░░░░░░░░  40%")
    time.sleep(6)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("INCREASING VOLTAGE +10,000v")
-   print("▓▓▓▓▓░░░░░  50%")
+   print("▓▓▓▓▓▓▓▓▓▓░░░░░░░░░░  50%")
    time.sleep(3)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("SUBJECT NON-RESPONSIVE")
-   print("▓▓▓▓▓▓░░░░  60%")
+   print("▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░  60%")
    time.sleep(4)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("INCREASING VOLTAGE +20,000v")
-   print("▓▓▓▓▓▓▓░░░  70%")
+   print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░  70%")
    time.sleep(3)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("WARNING: VOLTAGE TOLERANCE LIMIT REACHED")
-   print("▓▓▓▓▓▓▓▓░░  80%")
+   print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░  80%")
    time.sleep(4)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("WARNING: SUBJECT VITALS CRITICAL")
-   print("▓▓▓▓▓▓▓▓▓░  90%")
+   print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░  90%")
    time.sleep(3)
    
    sys.stderr.write("\x1b[2J\x1b[H")
    print("WARNING: TERMINATE PROGRAM")
-   print("▓▓▓▓▓▓▓▓▓▓ 100%")
+   print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ 100%")
    time.sleep(3)
 
 experiment_xxx()
