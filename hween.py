72290b72a5caeb684f3b5a1d5a8ed7543d264337
holotape <mjw555@gmail.com>
2016-10-16 15:30:31 -0400

    Add files via upload

--- hween.py
+++ hween.py
@@ -4,47 +4,47 @@ def experiment_xxx():
    print("Installing XXX...      "),
    time.sleep(1)
 
-   print("\033c")
+   print("\033c"),
    print("░░░░░░░░░░   0%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓░░░░░░░░░  10%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓░░░░░░░░  20%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓░░░░░░░  30%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓░░░░░░  40%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓▓░░░░░  50%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓▓▓░░░░  60%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓▓▓▓░░░  70%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓▓▓▓▓░░  80%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓▓▓▓▓▓░  90%"),
    time.sleep(1)
    
-   print("\033c")
+   print("\033c"),
    print("▓▓▓▓▓▓▓▓▓▓ 100%"),
    
 experiment_xxx()
