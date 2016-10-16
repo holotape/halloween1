c1b8e9f192a13a1a7106653ebdb949838b4c3a51
holotape <mjw555@gmail.com>
2016-10-16 18:13:46 -0400

    Add files via upload

--- hween.py
+++ hween.py
@@ -17,52 +17,64 @@ def experiment_xxx():
    time.sleep(10)
 
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("INJECTING X-SERUM")
    print("░░░░░░░░░░   0%")
-   time.sleep(1)
+   time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("INJECTING X-SERUM")
    print("▓░░░░░░░░░  10%")
-   time.sleep(1)
+   time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("MEASURING VITALS")
    print("▓▓░░░░░░░░  20%")
-   time.sleep(1)
+   time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("MEASURING VITALS")
    print("▓▓▓░░░░░░░  30%")
-   time.sleep(1)
+   time.sleep(2)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("SUBJECT NON-RESPONSIVE")
    print("▓▓▓▓░░░░░░  40%")
-   time.sleep(1)
+   time.sleep(6)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("INCREASING VOLTAGE +10,000v")
    print("▓▓▓▓▓░░░░░  50%")
-   time.sleep(1)
+   time.sleep(3)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("SUBJECT NON-RESPONSIVE")
    print("▓▓▓▓▓▓░░░░  60%")
-   time.sleep(1)
+   time.sleep(4)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("INCREASING VOLTAGE +20,000v")
    print("▓▓▓▓▓▓▓░░░  70%")
-   time.sleep(1)
+   time.sleep(3)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("WARNING: VOLTAGE TOLERANCE LIMIT REACHED")
    print("▓▓▓▓▓▓▓▓░░  80%")
-   time.sleep(1)
+   time.sleep(4)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("WARNING: SUBJECT VITALS CRITICAL")
    print("▓▓▓▓▓▓▓▓▓░  90%")
-   time.sleep(1)
+   time.sleep(3)
    
    sys.stderr.write("\x1b[2J\x1b[H")
+   print("WARNING: TERMINATE PROGRAM")
    print("▓▓▓▓▓▓▓▓▓▓ 100%")
-   
+   time.sleep(3)
+
 experiment_xxx()
-print "[UNSUCCESSFUL]"
+print "[UNSUCCESSFUL: SUBJECT LOST]"
 time.sleep(2)
-print "RECALCULATING"
+print "[RECALCULATING]"
 time.sleep(2)
 while line:
     print line
