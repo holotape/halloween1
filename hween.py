d8a21ccff84fa7344701a039c916de8556c31aa6
holotape <mjw555@gmail.com>
2016-10-16 15:54:28 -0400

    sys.stderr.write("\x1b[2J\x1b[H")

--- hween.py
+++ hween.py
@@ -1,53 +1,53 @@
 ﻿import sys, time, subprocess
 print "hello"
 time.sleep(2)
-print(chr(27) + "[2J")
+sys.stderr.write("\x1b[2J\x1b[H")
 
 def experiment_xxx():
    print("Installing XXX...      ")
    time.sleep(1)
 
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("░░░░░░░░░░   0%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓░░░░░░░░░  10%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓░░░░░░░░  20%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓░░░░░░░  30%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓░░░░░░  40%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓░░░░░  50%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓░░░░  60%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓░░░  70%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓▓░░  80%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓▓▓░  90%")
    time.sleep(1)
    
-   print(chr(27) + "[2J")
+   sys.stderr.write("\x1b[2J\x1b[H")
    print("▓▓▓▓▓▓▓▓▓▓ 100%")
    
 experiment_xxx()
