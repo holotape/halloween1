1de250aef82c977ede235a68033882d8a0478a5b
holotape <mjw555@gmail.com>
2016-10-16 14:41:03 -0400

    os.system('clear')

--- hween.py
+++ hween.py
@@ -1,50 +1,50 @@
-﻿import sys, time
+﻿import sys, time, os
 
 def experiment_xxx():
    print("Installing XXX...      "),
    time.sleep(1)
 
-   clear
+   os.system('clear')
    print("░░░░░░░░░░   0%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓░░░░░░░░░  10%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓░░░░░░░░  20%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓░░░░░░░  30%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓░░░░░░  40%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓▓░░░░░  50%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓▓▓░░░░  60%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓▓▓▓░░░  70%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓▓▓▓▓░░  80%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓▓▓▓▓▓░  90%"),
    time.sleep(1)
    
-   clear
+   os.system('clear')
    print("▓▓▓▓▓▓▓▓▓▓ 100%"),
    
 experiment_xxx()
